b41f6a7cdf513a8f0e2117a323ddf01ca7da88ff
Your Name <you@example.com>
2025-03-25 15:05:25 +0600

    Исправлены ошибки при регистрации и логине пользователей

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,10 +1,10 @@
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, redirect
 from pyexpat.errors import messages
-
+from django.contrib import messages
 from apps.users.forms import CustomUserCreationForm
-
+from django.contrib.auth import get_user_model
 
 def register_view(request):
     if request.method == 'Post':
@@ -16,13 +16,18 @@ def register_view(request):
         form = CustomUserCreationForm()
     return render(request, 'users/register.html', {'form': form})
 
+User = get_user_model()
+
 def login_view(request):
-    if request.method == 'Post':
-        form = AuthenticationForm(request, data=request.POST)
+    form = AuthenticationForm()
+    if request.method == "Post":
+        form = AuthenticationForm(data=request.POST)
         if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                messages.success(request, "OK")
-        return render(request, 'users/login.html', {'form': form})
+            user = form.get_user()
+            login(request, user)
+            return redirect("{% url 'main' %}")
+
+        messages.error(request, "Неверные учетные данные")
+
+    return render(request, "users/login.html", {"form": form})
+
